8a622d0edb3a60f96dc77aa74b22e65f5f0cd063
Arasupalanivel <arasu.pinnai@gmail.com>
2025-01-04 12:28:15 +0530

    project

---
+++ src/test/java/com/omrbranch/task1/Sample.java
@@ -0,0 +1,20 @@
+package com.omrbranch.task1;
+
+import java.util.Scanner;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		Scanner Scan=new Scanner(System.in);
+		Scanner s =new Scanner(System.in);
+		System.out.println("enter the employee name is");
+		String employeeName=Scan.next();
+		System.out.println("my employee name is  "+employeeName);
+		System.out.println("enter the employee name is");
+		String employeeNam=s.nextLine();
+		System.out.println("my employee name is "+employeeNam);
+		
+
+	}
+
+}
